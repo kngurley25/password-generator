2662d0d55781f156a0c2c1089ed4553d769768b7
kngurley <kngurley25@gmail.com>
2021-10-02 16:35:29 -0500

    added character codes

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,10 @@
 // Assignment code here
 
-// function to generator password
-var generatePassword = function() {
+// ASCII character codes
+var lowerCase = characterArray (97, 122)
+var upperCase = characterArray (65, 90)
+var numeric = characterArray (48, 57)
+var special = characterArray (33, 47)
 
   // prompt for password length
   var promptLength = window.prompt("Choose the quantity of characters for your password. Choose a number between 8 and 128.");
@@ -14,7 +17,15 @@ var generatePassword = function() {
     }
 
   // prompt for character selections
-  var promptCharacterUpper = window.confirm("Default password will be LOWERCASE letters. Would you like to include UPPERCASE letters for added security?");
+  var promptCharacterLower = window.confirm("Would you like to include LOWERCASE letters for added security? Select OK for YES, CANCEL for NO.");
+    if (promptCharacterLower) {
+      console.log("Password will include LOWER letters.");
+    }
+    else {
+      console.log("Password will NOT include LOWER letters.");
+    }
+  
+  var promptCharacterUpper = window.confirm("Would you like to include UPPERCASE letters for added security? Select OK for YES, CANCEL for NO.");
     if (promptCharacterUpper) {
       console.log("Password will include UPPERCASE letters.");
     }
@@ -22,7 +33,7 @@ var generatePassword = function() {
       console.log("Password will NOT include UPPERCASE letters.");
     }
 
-  var promptCharacterNumeric = window.confirm("Default password will be LOWERCASE letters. Would you like to include NUMERIC values for added security?");
+  var promptCharacterNumeric = window.confirm("Would you like to include NUMERIC values for added security? Select OK for YES, CANCEL for NO.");
     if (promptCharacterNumeric) {
       console.log("Password will include NUMERIC values.");
     }
@@ -30,13 +41,25 @@ var generatePassword = function() {
       console.log("Password will NOT include NUMERIC values.");
     }
 
-  var promptCharacterSpecial = window.confirm("Default password will be LOWERCASE letters. Would you like to include SPECIAL characters for added security?");
+  var promptCharacterSpecial = window.confirm("Would you like to include SPECIAL characters for added security? Select OK for YES, CANCEL for NO.");
     if (promptCharacterSpecial) {
       console.log("Password will include SPECIAL characters.");
     }
     else {
       console.log("Password will NOT include SPECIAL characters.");
     }
+
+// function to loop through characters
+var characterArray = function(max, min) {
+  for (var i=min; i <=max; i++) {
+    characterArray.push(i);
+  }
+  return characterArray;
+}
+
+// function to generator password
+var generatePassword = function() {
+  var 
 }
 
 
