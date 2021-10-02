7f1ec1211a9db2da7147004e85b63ce717903251
kngurley <kngurley25@gmail.com>
2021-10-02 14:28:33 -0500

    added password length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,19 @@
 // Assignment code here
 
+// function to generator password
+var generatePassword = function() {
+
+  // prompt for password length
+  var promptLength = window.prompt("Choose the quantity of characters for your password. Choose a number between 8 and 128.");
+    // validate prompt for password length
+    if (promptLength < 8 || promptLength > 128 || promptLength === "" || promptLength === null) {
+      window.alert("Response is not a valid option. Enter value between 8 and 128.");
+      return generatePassword();
+    }
+
+}
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
