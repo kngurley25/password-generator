8fbbcb305edf6e6ffe445fa00d6c1c063f3b9883
kngurley <kngurley25@gmail.com>
2021-10-02 18:38:29 -0500

    functiont to randomize char codes

--- Develop/script.js
+++ Develop/script.js
@@ -1,67 +1,62 @@
 // Assignment code here
 
-// ASCII character codes
-var lowerCase = characterArray (97, 122)
-var upperCase = characterArray (65, 90)
-var numeric = characterArray (48, 57)
-var special = characterArray (33, 47)
-
-  // prompt for password length
-  var promptLength = window.prompt("Choose the quantity of characters for your password. Choose a number between 8 and 128.");
-  console.log("Password length will be " + promptLength + " characters long.");
+// prompt for password length
+var promptLength = window.prompt("Choose the quantity of characters for your password. Choose a number between 8 and 128.");
+console.log("Password length will be " + promptLength + " characters long.");
   
   // validate prompt for password length
     if (promptLength < 8 || promptLength > 128 || promptLength === "" || promptLength === null) {
       window.alert("Response is not a valid option. Enter value between 8 and 128.");
-      return generatePassword();
     }
 
-  // prompt for character selections
-  var promptCharacterLower = window.confirm("Would you like to include LOWERCASE letters for added security? Select OK for YES, CANCEL for NO.");
-    if (promptCharacterLower) {
-      console.log("Password will include LOWER letters.");
+// prompt for character selections 
+var promptLower = window.confirm("Would you like to include LOWERCASE letters? Select OK for YES, CANCEL for NO.");
+    if (promptLower) {
+      console.log("Password will include LOWERCASE letters.");
     }
     else {
-      console.log("Password will NOT include LOWER letters.");
+      console.log("Password will NOT include LOWERCASE letters.");
     }
-  
-  var promptCharacterUpper = window.confirm("Would you like to include UPPERCASE letters for added security? Select OK for YES, CANCEL for NO.");
-    if (promptCharacterUpper) {
+
+var promptUpper = window.confirm("Would you like to include UPPERCASE values? Select OK for YES, CANCEL for NO.");
+    if (promptUpper) {
       console.log("Password will include UPPERCASE letters.");
     }
     else {
       console.log("Password will NOT include UPPERCASE letters.");
     }
 
-  var promptCharacterNumeric = window.confirm("Would you like to include NUMERIC values for added security? Select OK for YES, CANCEL for NO.");
-    if (promptCharacterNumeric) {
+var promptNumeric = window.confirm("Would you like to include NUMERIC values? Select OK for YES, CANCEL for NO.");
+    if (promptNumeric) {
       console.log("Password will include NUMERIC values.");
     }
     else {
       console.log("Password will NOT include NUMERIC values.");
     }
 
-  var promptCharacterSpecial = window.confirm("Would you like to include SPECIAL characters for added security? Select OK for YES, CANCEL for NO.");
-    if (promptCharacterSpecial) {
+  var promptSpecial = window.confirm("Would you like to include SPECIAL characters? Select OK for YES, CANCEL for NO.");
+    if (promptSpecial) {
       console.log("Password will include SPECIAL characters.");
     }
     else {
       console.log("Password will NOT include SPECIAL characters.");
     }
 
-// function to loop through characters
-var characterArray = function(max, min) {
-  for (var i=min; i <=max; i++) {
-    characterArray.push(i);
-  }
-  return characterArray;
+// generate random number from ASCII
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random()*(max-min)+min);
+  return value;
 }
+
+var lowercase = String.fromCharCode(randomNumber(97,122));
+var uppercase = String.fromCharCode(randomNumber(65,90));
+var numeric = String.fromCharCode(randomNumber(48,57));
+var special = String.fromCharCode(randomNumber(33,47));
+
+
 
 // function to generator password
-var generatePassword = function() {
-  var 
-}
-
+   
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
