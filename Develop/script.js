0c87accbc58a3b7466f1b1e5f775cc1d602a844f
kngurley <kngurley25@gmail.com>
2021-10-02 20:06:08 -0500

    added array for character options

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,8 @@
 // prompt for password length
 var promptLength = window.prompt("Choose the quantity of characters for your password. Choose a number between 8 and 128.");
 console.log("Password length will be " + promptLength + " characters long.");
-  
+var length = parseInt(promptLength);  
+
   // validate prompt for password length
     if (promptLength < 8 || promptLength > 128 || promptLength === "" || promptLength === null) {
       window.alert("Response is not a valid option. Enter value between 8 and 128.");
@@ -17,6 +18,7 @@ var promptLower = window.confirm("Would you like to include LOWERCASE letters? S
     else {
       console.log("Password will NOT include LOWERCASE letters.");
     }
+    var trueLower = (promptLower);
 
 var promptUpper = window.confirm("Would you like to include UPPERCASE values? Select OK for YES, CANCEL for NO.");
     if (promptUpper) {
@@ -25,6 +27,7 @@ var promptUpper = window.confirm("Would you like to include UPPERCASE values? Se
     else {
       console.log("Password will NOT include UPPERCASE letters.");
     }
+    var trueUpper = (promptUpper)
 
 var promptNumeric = window.confirm("Would you like to include NUMERIC values? Select OK for YES, CANCEL for NO.");
     if (promptNumeric) {
@@ -33,6 +36,7 @@ var promptNumeric = window.confirm("Would you like to include NUMERIC values? Se
     else {
       console.log("Password will NOT include NUMERIC values.");
     }
+    var trueNumeric = (promptNumeric)
 
   var promptSpecial = window.confirm("Would you like to include SPECIAL characters? Select OK for YES, CANCEL for NO.");
     if (promptSpecial) {
@@ -41,22 +45,51 @@ var promptNumeric = window.confirm("Would you like to include NUMERIC values? Se
     else {
       console.log("Password will NOT include SPECIAL characters.");
     }
+    var trueSpecial = (promptSpecial)
 
-// generate random number from ASCII
+// console.log(trueLower, trueUpper, trueNumeric, trueSpecial);
+
+// function to generator password
+
+var finalPassword = generatePassword (length, trueLower, trueUpper, trueNumeric, trueSpecial) 
+
+function generatePassword (length, lowercase, uppercase, numeric, special) {
+  var genPassword = "";
+  var typeCount = lowercase + uppercase + numeric + special;
+
+  var typeArray = [{lowercase}, {uppercase}, {numeric}, {special}].filter(
+    item => Object.values(item)[0]
+  );
+  console.log(typeArray);
+
+}
+
+var characterSelect = {
+  lowercase: genLowercase,
+  uppercase: genUppercase,
+  numeric: genNumeric,
+  special: genSpecial,
+}
+
+// generate random number from ASCII char code range of values
 var randomNumber = function(min, max) {
   var value = Math.floor(Math.random()*(max-min)+min);
   return value;
 }
 
-var lowercase = String.fromCharCode(randomNumber(97,122));
-var uppercase = String.fromCharCode(randomNumber(65,90));
-var numeric = String.fromCharCode(randomNumber(48,57));
-var special = String.fromCharCode(randomNumber(33,47));
-
-
-
-// function to generator password
-   
+var genLowercase = function () {
+  return String.fromCharCode(randomNumber(97,122));
+}
+var genUppercase = function () {
+  return String.fromCharCode(randomNumber(65,90));
+}
+var genNumeric = function () {
+  return String.fromCharCode(randomNumber(48,57));
+}
+var genSpecial = function () {
+  return String.fromCharCode(randomNumber(33,47));
+}
+ 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -67,7 +100,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
