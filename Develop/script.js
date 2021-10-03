cd19c063bcae70f583db6527be092d02919ab486
kngurley <kngurley25@gmail.com>
2021-10-03 12:59:50 -0500

    general code cleanup

--- Develop/script.js
+++ Develop/script.js
@@ -3,115 +3,116 @@
 // function to generate password
 function generatePassword() {
 
-// prompt for password length
-var passLength = function () {
-  var promptLength = window.prompt("Choose the quantity of characters for your password. Choose a number between 8 and 128.");
-  console.log("Password length will be " + promptLength + " characters long.");
-  
-    // validate prompt for password length
-    if (promptLength < 8 || promptLength > 128 || promptLength === "" || promptLength === null) {
-      window.alert("Response is not a valid option. Enter value between 8 and 128.");
-    return passLength();
+  // prompt for password length
+  var passLength = function () {
+    var promptLength = window.prompt("Choose the quantity of characters for your password. Choose a number between 8 and 128.");
+    
+      // validate prompt for password length
+      if (promptLength < 8 || promptLength > 128 || promptLength === "" || promptLength === null) {
+        window.alert("Response is not a valid option. Enter value between 8 and 128.");
+      return passLength();
+      }
+      
+    console.log("Password length will be " + promptLength + " characters long.");  
+    return promptLength;
     }
-  return promptLength;
-  }
-var length = passLength();
+    var length = passLength();
 
-// prompts for character selections 
-var promptLower = window.confirm("Would you like to include LOWERCASE letters? Select OK for YES, CANCEL for NO.");
-    if (promptLower) {
-      console.log("Password will include LOWERCASE letters.");
-    }
-    else {
-      console.log("Password will NOT include LOWERCASE letters.");
-    }
+  // prompts for character selections 
+  var promptLower = window.confirm("Would you like to include LOWERCASE letters? Select OK for YES, CANCEL for NO.");
+      if (promptLower) {
+        console.log("Password will include LOWERCASE letters.");
+      }
+      else {
+        console.log("Password will NOT include LOWERCASE letters.");
+      }
     var trueLower = (promptLower);
 
-var promptUpper = window.confirm("Would you like to include UPPERCASE values? Select OK for YES, CANCEL for NO.");
-    if (promptUpper) {
-      console.log("Password will include UPPERCASE letters.");
-    }
-    else {
-      console.log("Password will NOT include UPPERCASE letters.");
-    }
+  var promptUpper = window.confirm("Would you like to include UPPERCASE values? Select OK for YES, CANCEL for NO.");
+      if (promptUpper) {
+        console.log("Password will include UPPERCASE letters.");
+      }
+      else {
+        console.log("Password will NOT include UPPERCASE letters.");
+      }
     var trueUpper = (promptUpper);
 
-var promptNumeric = window.confirm("Would you like to include NUMERIC values? Select OK for YES, CANCEL for NO.");
-    if (promptNumeric) {
-      console.log("Password will include NUMERIC values.");
-    }
-    else {
-      console.log("Password will NOT include NUMERIC values.");
-    }
+  var promptNumeric = window.confirm("Would you like to include NUMERIC values? Select OK for YES, CANCEL for NO.");
+      if (promptNumeric) {
+        console.log("Password will include NUMERIC values.");
+      }
+      else {
+        console.log("Password will NOT include NUMERIC values.");
+      }
     var trueNumeric = (promptNumeric);
 
   var promptSpecial = window.confirm("Would you like to include SPECIAL characters? Select OK for YES, CANCEL for NO.");
-    if (promptSpecial) {
-      console.log("Password will include SPECIAL characters.");
-    }
-    else {
-      console.log("Password will NOT include SPECIAL characters.");
-    }
+      if (promptSpecial) {
+        console.log("Password will include SPECIAL characters.");
+      }
+      else {
+        console.log("Password will NOT include SPECIAL characters.");
+      }
     var trueSpecial = (promptSpecial);
 
-// console.log(trueLower, trueUpper, trueNumeric, trueSpecial);
+  // console.log(trueLower, trueUpper, trueNumeric, trueSpecial);
 
-// generate random number from ASCII char code range of values
-var randomNumber = function(min, max) {
-  var value = Math.floor(Math.random()*(max-min)+min);
-  return value;
-}
-
-var genLowercase = function () {
-  return String.fromCharCode(randomNumber(97,122));
-}
-var genUppercase = function () {
-  return String.fromCharCode(randomNumber(65,90));
-}
-var genNumeric = function () {
-  return String.fromCharCode(randomNumber(48,57));
-}
-var genSpecial = function () {
-  return String.fromCharCode(randomNumber(33,47));
-}
-
-// object key reference for generated random numbers
-var characterSelect = { 
-  lowercase: genLowercase,
-  uppercase: genUppercase,
-  numeric: genNumeric,
-  special: genSpecial
-};
-
-// function to select characters
-var finalPassword = passwordGen (length, trueLower, trueUpper, trueNumeric, trueSpecial)
-
-function passwordGen (length, lowercase, uppercase, numeric, special) {
-  var password = "";
-  
-  // verify which type of characters are selected
-  var typeCount = lowercase + uppercase + numeric + special;
-  var typeArray = [{lowercase}, {uppercase}, {numeric}, {special}].filter(
-    item => Object.values(item)[0]
-  );
-  // console.log(typeArray);
-  
-  if (typeCount === 0) {
-    password = "No characters selected. Please try again.";
+  // generate random number from ASCII char code range of values
+  var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random()*(max-min)+min);
+    return value;
   }
 
-  // loop through each type of character selected
-  for (var i = 0; i < length; i += typeCount) {
-    typeArray.forEach(type => {
-      var character = Object.keys(type)[0];
-      // console.log(character);
-      password += characterSelect[character]();
-    })
+      var genLowercase = function () {
+        return String.fromCharCode(randomNumber(97,122));
+      }
+      var genUppercase = function () {
+        return String.fromCharCode(randomNumber(65,90));
+      }
+      var genNumeric = function () {
+        return String.fromCharCode(randomNumber(48,57));
+      }
+      var genSpecial = function () {
+        return String.fromCharCode(randomNumber(33,47));
+      }
+
+      // object key reference for generated random numbers
+      var characterSelect = { 
+        lowercase: genLowercase,
+        uppercase: genUppercase,
+        numeric: genNumeric,
+        special: genSpecial
+      };
+
+  // function to select characters
+  var finalPassword = passwordGen (length, trueLower, trueUpper, trueNumeric, trueSpecial)
+
+  function passwordGen (length, lowercase, uppercase, numeric, special) {
+    var password = "";
+    
+    // verify which type of characters are selected
+    var typeCount = lowercase + uppercase + numeric + special;
+    var typeArray = [{lowercase}, {uppercase}, {numeric}, {special}].filter(
+      item => Object.values(item)[0]
+    );
+    // console.log(typeArray);
+    
+    if (typeCount === 0) {
+      password = "No characters selected. Please try again.";
+    }
+
+    // loop through each type of character selected
+    for (var i = 0; i < length; i += typeCount) {
+      typeArray.forEach(type => {
+        var character = Object.keys(type)[0];
+        // console.log(character);
+        password += characterSelect[character]();
+      })
+    }
+    console.log("Your password is: " + password);
+    return password;
   }
-  console.log(password);
-  return password;
-}
-return finalPassword;
+  return finalPassword;
 }
 
 // Get references to the #generate element
